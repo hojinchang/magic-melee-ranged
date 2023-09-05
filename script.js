d1660c9f09e709943dc8a9a19c3e109eb6b1005b
hojinchang <hojinchang1998@gmail.com>
2023-09-04 20:13:15 -0700

    Add function which plays a single round
    Add function that plays a single round of rock paper scissors.
    The previous computer random choice and input user choice is compared to decide the winner.
    A switch statement is used to determine the output winner/loser message.

--- script.js
+++ script.js
@@ -4,3 +4,40 @@ function getComputerChoice() {
     
     return choices[idxChoice];
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    let declareWinner;
+    let roundDecision;
+
+    if (playerSelection === computerSelection) {
+        roundDecision = "tie";
+    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+                playerSelection === "paper" && computerSelection == "rock" ||
+                playerSelection == "scissors" && computerSelection == "paper") {
+
+        roundDecision = "player";
+    } else {
+        roundDecision = "computer";
+    }
+
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+
+    switch(roundDecision) {
+        case "tie":
+            declareWinner = `It's a tie! ${playerSelection} ties with ${computerSelection}!`;
+            break;
+        
+        case "player":
+            declareWinner = `You won! ${playerSelection} beats ${computerSelection}`;
+            break;
+
+        case "computer":
+            declareWinner = `You lost! ${playerSelection} loses to ${computerSelection}`;
+            break;
+    }
+
+    return declareWinner;
+}
